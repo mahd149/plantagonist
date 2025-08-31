5e08ac6e964878cbd64c891be76accb782734109
mahd149 <sahil20030219@gmail.com>
2025-08-31 19:52:22 +0600

    app runs once again phew

--- src/main/java/org/plantagonist/App.java
+++ src/main/java/org/plantagonist/App.java
@@ -8,6 +8,13 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // at the top of start()
+//        var url = App.class.getResource("/org/plantagonist/ui/main.fxml");
+//        System.out.println("FXML URL -> " + url);
+//        var loader = new javafx.fxml.FXMLLoader(java.util.Objects.requireNonNull(
+//                url, "Missing on classpath: /org/plantagonist/ui/main.fxml"
+//        ));
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/plantagonist/ui/main.fxml"));
         Scene scene = new Scene(loader.load(), 1080, 700);
         stage.setTitle("Plantagonist");
